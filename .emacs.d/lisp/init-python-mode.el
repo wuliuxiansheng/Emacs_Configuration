5c6de9b7c07f1d3fb3a4dfbe5ab01d899287c5ef
Chao Liu <chao.liu0307@gmail.com>
2015-06-07 17:57:17 -0400

    edit init-python-mode.el to configure ipython shell to be the interpreter for python mode

--- .emacs.d/lisp/init-python-mode.el
+++ .emacs.d/lisp/init-python-mode.el
@@ -5,5 +5,16 @@
 
 (require-package 'pip-requirements)
 
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+ "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+ "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+ "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 (provide 'init-python-mode)
