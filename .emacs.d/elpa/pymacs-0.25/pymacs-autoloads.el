f3503e75a7081f45bc5f0403c08b1154a0219857
Chao Liu <chao.liu0307@gmail.com>
2015-06-09 18:08:05 -0400

    add pymacs package to elpa folder

---
+++ .emacs.d/elpa/pymacs-0.25/pymacs-autoloads.el
@@ -0,0 +1,64 @@
+;;; pymacs-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "pymacs" "pymacs.el" (21877 59613 0 0))
+;;; Generated autoloads from pymacs.el
+
+(autoload 'pymacs-load "pymacs" "\
+Import the Python module named MODULE into Emacs.
+Each function in the Python module is made available as an Emacs function.
+The Lisp name of each function is the concatenation of PREFIX with
+the Python name, in which underlines are replaced by dashes.  If PREFIX is
+not given, it defaults to MODULE followed by a dash.
+If NOERROR is not nil, do not raise error when the module is not found.
+
+\(fn MODULE &optional PREFIX NOERROR)" t nil)
+
+(autoload 'pymacs-autoload "pymacs" "\
+Pymacs's equivalent of the standard emacs facility `autoload'.
+Define FUNCTION to autoload from Python MODULE using PREFIX.
+If PREFIX is not given, it defaults to MODULE followed by a dash.
+Optional DOCSTRING documents FUNCTION until it gets loaded.
+INTERACTIVE is normally the argument to the function `interactive',
+t means `interactive' without arguments, nil means not interactive,
+which is the default.
+
+\(fn FUNCTION MODULE &optional PREFIX DOCSTRING INTERACTIVE)" nil nil)
+
+(autoload 'pymacs-eval "pymacs" "\
+Compile TEXT as a Python expression, and return its value.
+
+\(fn TEXT)" t nil)
+
+(autoload 'pymacs-exec "pymacs" "\
+Compile and execute TEXT as a sequence of Python statements.
+This functionality is experimental, and does not appear to be useful.
+
+\(fn TEXT)" t nil)
+
+(autoload 'pymacs-call "pymacs" "\
+Return the result of calling a Python function FUNCTION over ARGUMENTS.
+FUNCTION is a string denoting the Python function, ARGUMENTS are separate
+Lisp expressions, one per argument.  Immutable Lisp constants are converted
+to Python equivalents, other structures are converted into Lisp handles.
+
+\(fn FUNCTION &rest ARGUMENTS)" nil nil)
+
+(autoload 'pymacs-apply "pymacs" "\
+Return the result of calling a Python function FUNCTION over ARGUMENTS.
+FUNCTION is a string denoting the Python function, ARGUMENTS is a list of
+Lisp expressions.  Immutable Lisp constants are converted to Python
+equivalents, other structures are converted into Lisp handles.
+
+\(fn FUNCTION ARGUMENTS)" nil nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pymacs-autoloads.el ends here
---
+++ .emacs.d/elpa/pymacs-0.25/pymacs-pkg.el
@@ -0,0 +1,1 @@
+(define-package "pymacs" "0.25" "Interface between Emacs Lisp and Python" 'nil)
