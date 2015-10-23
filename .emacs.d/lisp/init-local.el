335a7e27da44ab6d6ce069d76922fdf27fd1a0f1
Chao Liu <chao.liu0307@gmail.com>
2015-10-22 21:33:34 -0400

    add default font configuration in init-local.el

--- .emacs.d/lisp/init-local.el
+++ .emacs.d/lisp/init-local.el
@@ -2,6 +2,12 @@
 (setq user-full-name "Chao Liu")
 (setq user-mail-address "chao.liu0307@gmail.com")
 
+;;;set the default font and size
+(when *is-a-mac*
+  (set-default-font "-*-Menlo-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1"))
+(when *is-a-linux*
+  (set-default-font "-unkonwn-Ubuntu Mono-normal-normal-normal-*-19-*-*-*-m-0-iso10646-1"))
+
 (global-linum-mode t);show line number
 
 ;;;cursor stop blinking 
