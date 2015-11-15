c17fd6e03828ee89b8bfb64fb5d088be96078007
Chao Liu <chao.liu0307@gmail.com>
2015-11-15 11:30:32 -0500

    modify init-preload-local.el
    move the alarm turning-off part into (when *is-a-mac*)

--- .emacs.d/lisp/init-preload-local.el
+++ .emacs.d/lisp/init-preload-local.el
@@ -1,12 +1,13 @@
 ;;;set the default font and size
 (when *is-a-mac*
-  (set-frame-font "-*-Menlo-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1"))
+  (set-frame-font "-*-Menlo-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
+  (setq ring-bell-function 'ignore);turn the alarm totally off because of the bug in Capitan EI
+  )
 (when *is-a-linux*
   (set-frame-font "-unkonwn-Ubuntu Mono-normal-normal-normal-*-19-*-*-*-m-0-iso10646-1"))
 (when *is-a-windows*
   (set-frame-font "-outline-Courier New-normal-normal-normal-mono-16-*-*-*-c-*-iso8859-1"))
 
-(setq ring-bell-function 'ignore);turn the alarm totally off
 (global-linum-mode t);show line number
 
 ;;;cursor stop blinking
