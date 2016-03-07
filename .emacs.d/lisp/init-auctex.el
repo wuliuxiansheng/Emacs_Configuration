9cf8c537f93dc3ddff49ee64ca25586f4e9b5093
Chao Liu <chao.liu0307@gmail.com>
2016-03-06 22:34:42 -0500

    Add Okular configuration for Linux Auctex
    Configure Okular to be PDF viewer for forward and inverse search and add
    option --synctex==1 to xelatex to generate .synctex.gz for forward and
    inverse search.

--- .emacs.d/lisp/init-auctex.el
+++ .emacs.d/lisp/init-auctex.el
@@ -60,11 +60,11 @@
   (setq TeX-parse-self t)
   (setq-default TeX-master nil)
 
-  (setq TeX-output-view-style (quote (("^pdf$" "." "evince %o %(outpage)"))))
+  (setq TeX-output-view-style '(("^pdf$" "." "okular %o %(outpage)")))
 
   (add-hook 'LaTeX-mode-hook
 			(lambda()
-			  (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
+			  (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex --synctex=1%(mode)%' %t" TeX-run-TeX nil t))
 			  (setq TeX-command-default "XeLaTeX")))
 
   (mapc (lambda (mode)
@@ -85,11 +85,39 @@
 			  ))
   (setq TeX-source-correlate-mode t)
   (setq TeX-source-correlate-method 'synctex)
-  (setq TeX-view-program-list
-		'(("Evince" "evince %o")))
-  (setq TeX-view-program-selection
-		'((output-pdf "Evince")
-		  (output-dvi "xdvi")))
+
+  ;; Configure Evince as PDF viewer
+  ;; (setq TeX-view-program-list
+  ;;		'(("Evince" "evince %o")))
+  ;; (setq TeX-view-program-selection
+  ;;		'((output-pdf "Evince")
+  ;;		  (output-dvi "xdvi")))
+
+  ;; Configure Okular as PDF viewer for forward search and inverse search
+  (add-hook 'LaTeX-mode-hook '(lambda ()
+								(add-to-list 'TeX-expand-list
+											 '("%u" Okular-make-url))))
+  (defun Okular-make-url ()
+	(concat
+	 "file://"
+	 (expand-file-name (funcall file (TeX-output-extension) t)
+					   (file-name-directory (TeX-master-file)))
+	 "#src:"
+	 (TeX-current-line)
+	 (expand-file-name (TeX-master-directory))
+	 "./"
+	 (TeX-current-file-name-master-relative)))
+  (setq TeX-view-program-list '(("Okular"  ("okular --unique %u"))))
+  (setq TeX-view-program-selection '((output-pdf "Okular")
+									 (output-dvi "xdvi")))
+
+  ;; (setq TeX-view-program-list
+  ;;		(quote (("Okular" "okular --unique %o#src:%n%b"))))
+  ;; (setq TeX-view-program-selection
+  ;;		(quote ((engine-omega "dvips and gv")
+  ;;				(output-dvi "xdvi")
+  ;;				(output-pdf "Okular"))))
+
   (setq TeX-insert-quote t)
   (ispell-change-dictionary "american" t)
   (setq-default ispell-program-name "aspell")
