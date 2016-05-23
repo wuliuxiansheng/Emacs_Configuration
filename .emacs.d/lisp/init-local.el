211f4e096cd754d1b4bc3fff3303d9d3e23b7553
Chao Liu <chaoliu@seas.upenn.edu>
2016-05-22 22:00:53 -0400

    Disable M-SPC for set-mark-command
    Windows 10 use Windows + SPC to change input method, so revert back to
    use C-SPC for set-mark-command.

--- .emacs.d/lisp/init-local.el
+++ .emacs.d/lisp/init-local.el
@@ -3,9 +3,9 @@
 (setq user-mail-address "chaoliu@seas.upenn.edu")
 
 ;;; bind M-SPC to set-mark command rather than C-SPC for Windows
-(when *is-a-windows*
-  (global-unset-key (kbd "C-SPC"))
-  (global-set-key (kbd "M-SPC") 'set-mark-command))
+;; (when *is-a-windows*
+;;   (global-unset-key (kbd "C-SPC"))
+;;   (global-set-key (kbd "M-SPC") 'set-mark-command))
 
 ;;; code hide
 (load-library "hideshow")
