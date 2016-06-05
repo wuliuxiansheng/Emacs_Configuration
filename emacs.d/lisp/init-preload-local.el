0b2fc2fc8e425b5e59558e4fbc23257d05458d00
Chao Liu <chao.liu0307@gmail.com>
2016-06-05 15:25:49 -0400

    revert ring-bell-function for Capitan EI
    The display issue in Capitan EI is fixed.

--- emacs.d/lisp/init-preload-local.el
+++ emacs.d/lisp/init-preload-local.el
@@ -1,7 +1,7 @@
 ;;;set the default font and size
 (when *is-a-mac*
   (set-frame-font "-*-Menlo-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
-  (setq ring-bell-function 'ignore);turn the alarm totally off because of the bug in Capitan EI
+  ;; (setq ring-bell-function 'ignore);turn the alarm totally off because of the bug in Capitan EI
   )
 (when *is-a-linux*
   (set-frame-font "-unkonwn-Ubuntu Mono-normal-normal-normal-*-19-*-*-*-m-0-iso10646-1"))
