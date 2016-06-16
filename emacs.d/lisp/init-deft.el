7ff9ec7e0ed98a7f031dc4e50dc1bcac9e52c596
Chao Liu <chao.liu0307@gmail.com>
2016-06-16 13:17:20 -0400

    Update deft configuration
    Change key-binding for searching notes using deft-find-file and disable
    auto-save because of trailing whitespace configuration. When saving
    files, Emacs will delete all trailing whitespace which is annoying if
    auto-save happen every 1 second when typing.

--- emacs.d/lisp/init-deft.el
+++ emacs.d/lisp/init-deft.el
@@ -4,6 +4,7 @@
 
 (setq deft-extensions '("org" "txt"))
 (setq deft-directory "~/Dropbox/OrgNotes")
+(setq deft-text-mode 'org-mode)
 
 ;; Enable subdirectories search
 (setq deft-recursive t)
@@ -14,12 +15,17 @@
 (setq deft-use-filename-as-title t)
 (setq deft-use-filter-string-for-filename t)
 
+;; disable auto save because of trailing whitespace configuration
+(setq deft-auto-save-interval 0)
+
 (setq deft-markdown-mode-title-level 2)
 (setq deft-org-mode-title-prefix t)
+
+(setq deft-incremental-search nil)
 
 (setq deft-current-sort-method 'title)
 
 ;; global key binding to find files in deft directory
-(global-set-key (kbd "C-x C-s") 'deft-find-file)
+(global-set-key (kbd "C-c d") 'deft-find-file)
 
 (provide 'init-deft)
