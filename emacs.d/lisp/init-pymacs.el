b24943054f356ea407536b3103b7e08e218619b7
Chao Liu <chao.liu0307@gmail.com>
2016-06-18 20:03:36 -0400

    Add ropemacs to company backend in init-pymacs.el

--- emacs.d/lisp/init-pymacs.el
+++ emacs.d/lisp/init-pymacs.el
@@ -2,20 +2,22 @@
 
 ;;; pymacs configuration
 (eval-after-load 'python
-'(progn
-  (autoload 'pymacs-apply "pymacs")
-  (autoload 'pymacs-call "pymacs")
-  (autoload 'pymacs-eval "pymacs" nil t)
-  (autoload 'pymacs-exec "pymacs" nil t)
-  (autoload 'pymacs-load "pymacs" nil t)
-  (message "loading ropemacs")
-  (pymacs-load "ropemacs" "rope-")
-  (setq ropemacs-enable-autoimport t)))
+  '(progn
+	 (autoload 'pymacs-apply "pymacs")
+	 (autoload 'pymacs-call "pymacs")
+	 (autoload 'pymacs-eval "pymacs" nil t)
+	 (autoload 'pymacs-exec "pymacs" nil t)
+	 (autoload 'pymacs-load "pymacs" nil t)
+	 (message "loading ropemacs")
+	 (pymacs-load "ropemacs" "rope-")
+	 (setq ropemacs-enable-autoimport t)))
 ;;; integrate ropemacs with auto-complete
 ;;(ac-ropemacs-initialize)
 ;; (add-hook 'python-mode-hook
 ;; (lambda ()
 ;;   (add-to-list 'ac-sources 'ac-source-ropemacs)))
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-ropemacs))
 
 
 (provide 'init-pymacs)
