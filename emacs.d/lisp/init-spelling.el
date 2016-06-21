7acc141fb73092464cbc80fb77ce1a4d269d2027
Chao Liu <chao.liu0307@gmail.com>
2016-06-20 22:15:12 -0400

    Use M-c to do ispell-word command

--- emacs.d/lisp/init-spelling.el
+++ emacs.d/lisp/init-spelling.el
@@ -1,5 +1,8 @@
 (require 'ispell)
 
+;; check and correct spelling of the word at point or a region
+(global-set-key (kbd "M-c") 'ispell-word)
+
 (when (executable-find ispell-program-name)
   (require 'init-flyspell))
 
