d459ebaf8746d8e310487a45eb6901fdf7c778ec
Chao Liu <chaoliu@seas.upenn.edu>
2016-06-28 16:42:19 -0400

    Change default character size for Linux and Windows

--- emacs.d/lisp/init-preload-local.el
+++ emacs.d/lisp/init-preload-local.el
@@ -5,10 +5,10 @@
   ;; (setq ring-bell-function 'ignore);turn the alarm totally off because of the bug in Capitan EI
   )
 (when *is-a-linux*
-  (set-frame-font "-unkonwn-Ubuntu Mono-normal-normal-normal-*-19-*-*-*-m-0-iso10646-1")
+  (set-frame-font "-unkonwn-Ubuntu Mono-normal-normal-normal-*-18-*-*-*-m-0-iso10646-1")
   (setq face-font-rescale-alist '(("Droid Sans" . 1.1)))) ;; configure Chinese characters to align tables
 (when *is-a-windows*
-  (set-frame-font "-outline-Courier New-normal-normal-normal-mono-16-*-*-*-c-*-iso8859-1")
+  (set-frame-font "-outline-Courier New-normal-normal-normal-mono-14-*-*-*-c-*-iso8859-1")
   (setq face-font-rescale-alist '(("NSimsun" . 1.2) ("MS Gothic" . 1.2))) ;; configure Chinese characters to align tables
   (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/")  ; add aspell directory to emacs configuration
   (setq grep-find-command '("findstr /sn  *" . 13))
