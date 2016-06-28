2399184cdaef06439dd10fbb3658a5c84dae31fd
Chao Liu <chao.liu0307@gmail.com>
2016-06-28 16:16:55 -0400

    Add some comments to font configuration

--- emacs.d/lisp/init-preload-local.el
+++ emacs.d/lisp/init-preload-local.el
@@ -1,15 +1,15 @@
 ;;;set the default font and size
 (when *is-a-mac*
   (set-frame-font "-*-Menlo-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1")
-  (setq face-font-rescale-alist '(("MS" . 1.2)))
+  (setq face-font-rescale-alist '(("MS" . 1.2))) ;; configure Chinese characters to align tables
   ;; (setq ring-bell-function 'ignore);turn the alarm totally off because of the bug in Capitan EI
   )
 (when *is-a-linux*
   (set-frame-font "-unkonwn-Ubuntu Mono-normal-normal-normal-*-19-*-*-*-m-0-iso10646-1")
-  (setq face-font-rescale-alist '(("Droid Sans" . 1.1))))
+  (setq face-font-rescale-alist '(("Droid Sans" . 1.1)))) ;; configure Chinese characters to align tables
 (when *is-a-windows*
   (set-frame-font "-outline-Courier New-normal-normal-normal-mono-16-*-*-*-c-*-iso8859-1")
-  (setq face-font-rescale-alist '(("NSimsun" . 1.2) ("MS Gothic" . 1.2)))
+  (setq face-font-rescale-alist '(("NSimsun" . 1.2) ("MS Gothic" . 1.2))) ;; configure Chinese characters to align tables
   (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/")  ; add aspell directory to emacs configuration
   (setq grep-find-command '("findstr /sn  *" . 13))
   (setq default-directory "~/"))
