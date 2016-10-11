d3ba4c80b04c4ed1fbdd50fe9f76ecd4995fe756
Chao Liu <chaoliu@seas.upenn.edu>
2016-10-10 22:50:10 -0400

    Update python configuration
    1. Add anaconda-mode for Python configuration
    2. Disable company-mode for Pymacs
    3. Change IPython configuration. It's only working for IPython 4.2.0 now.

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -6,10 +6,20 @@
 
 (require-package 'pip-requirements)
 
-;; ;;; iPython configuration
+;;; Configure anaconda-mode
+(when (maybe-require-package 'anaconda-mode)
+  (after-load 'python
+    (add-hook 'python-mode-hook 'anaconda-mode)
+    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+  (when (maybe-require-package 'company-anaconda)
+    (after-load 'company
+      (add-hook 'python-mode-hook
+                (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
+
+;;; iPython configuration
 (setq
  python-shell-interpreter "ipython"
- python-shell-interpreter-args "--simple-prompt"  ;; required by IPython 5
+ python-shell-interpreter-args ""  ;; required by IPython 5
  ;; python-shell-prompt-regexp "In \\[[0-9]+\\]: "
  ;; python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
  ;; python-shell-completion-setup-code
@@ -42,8 +52,8 @@
 	 (pymacs-load "ropemacs" "rope-")
 	 (setq ropemacs-enable-autoimport t)))
 ;;; integrate ropemacs with company-mode
-(defun company-ropemacs-setup ()
-  (add-to-list 'company-backend 'company-ropemacs))
-(add-hook 'python-mode-hook 'company-ropemacs-setup)
+;; (defun company-ropemacs-setup ()
+;;   (add-to-list 'company-backend 'company-ropemacs))
+;; (add-hook 'python-mode-hook 'company-ropemacs-setup)
 
 (provide 'init-python-mode)
