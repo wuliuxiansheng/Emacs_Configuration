203a378c7f34fa63b16156ee5264cda3fe92dbc2
Chao Liu <chaoliu@seas.upenn.edu>
2016-10-14 23:55:26 -0400

    Disable Pymacs configuration for Python mode
    May remove all Pymacs configuration in the future but anaconda-mode needs
    more tests.

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -39,18 +39,18 @@
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
 ;;; pymacs configuration
-(require-package 'pymacs)
+;; (require-package 'pymacs)
 
-(eval-after-load 'python
-  '(progn
-	 (autoload 'pymacs-apply "pymacs")
-	 (autoload 'pymacs-call "pymacs")
-	 (autoload 'pymacs-eval "pymacs" nil t)
-	 (autoload 'pymacs-exec "pymacs" nil t)
-	 (autoload 'pymacs-load "pymacs" nil t)
-	 (message "loading ropemacs")
-	 (pymacs-load "ropemacs" "rope-")
-	 (setq ropemacs-enable-autoimport t)))
+;; (eval-after-load 'python
+;;   '(progn
+;; 	 (autoload 'pymacs-apply "pymacs")
+;; 	 (autoload 'pymacs-call "pymacs")
+;; 	 (autoload 'pymacs-eval "pymacs" nil t)
+;; 	 (autoload 'pymacs-exec "pymacs" nil t)
+;; 	 (autoload 'pymacs-load "pymacs" nil t)
+;; 	 (message "loading ropemacs")
+;; 	 (pymacs-load "ropemacs" "rope-")
+;; 	 (setq ropemacs-enable-autoimport t)))
 ;;; integrate ropemacs with company-mode
 ;; (defun company-ropemacs-setup ()
 ;;   (add-to-list 'company-backend 'company-ropemacs))
