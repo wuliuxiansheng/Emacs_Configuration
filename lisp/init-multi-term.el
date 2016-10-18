2ed9525dac414a619b15391991dccb5798a63eaf
Chao Liu <chao.liu0307@gmail.com>
2016-10-17 23:34:50 -0400

    Update multi-term configuration
    Enable multi-term to show maximum output when the window is
    scrolled.

--- lisp/init-multi-term.el
+++ lisp/init-multi-term.el
@@ -8,6 +8,7 @@
 (when *is-a-mac*
   (setq system-uses-terminfo nil)  ;; fix the display problem in Mac
   )
+(setq multi-term-scroll-show-maximum-output t)  ;; show the maximum output when the window is scrolled
 
 (global-set-key (kbd "C-x M") 'multi-term)
 
