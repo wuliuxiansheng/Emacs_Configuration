53afa460fb7e3de8822182a2591211fb08a2c84f
Chao Liu <chaoliu@seas.upenn.edu>
2017-03-04 17:26:21 -0500

    Use pymacs and ropemacs rather than anaconda mode for python

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -7,20 +7,20 @@
 (require-package 'pip-requirements)
 
 ;;; Configure anaconda-mode
-(when (maybe-require-package 'anaconda-mode)
-  (after-load 'python
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
-  (when (maybe-require-package 'company-anaconda)
-    (after-load 'company
-      (add-hook 'python-mode-hook
-                (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
+;; (when (maybe-require-package 'anaconda-mode)
+;;   (after-load 'python
+;;     (add-hook 'python-mode-hook 'anaconda-mode)
+;;     (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+;;   (when (maybe-require-package 'company-anaconda)
+;;     (after-load 'company
+;;       (add-hook 'python-mode-hook
+;;                 (lambda () (sanityinc/local-push-company-backend 'company-anaconda))))))
 
 ;;; iPython configuration
 (setq
  python-shell-interpreter "ipython"
  python-shell-interpreter-args ""  ;; required by IPython 5
- python-shell-completion-native-enable nil
+ ;; python-shell-completion-native-enable nil
  ;; python-shell-prompt-regexp "In \\[[0-9]+\\]: "
  ;; python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
  ;; python-shell-completion-setup-code
@@ -40,21 +40,21 @@
 (add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
 ;;; pymacs configuration
-;; (require-package 'pymacs)
+(require-package 'pymacs)
 
-;; (eval-after-load 'python
-;;   '(progn
-;; 	 (autoload 'pymacs-apply "pymacs")
-;; 	 (autoload 'pymacs-call "pymacs")
-;; 	 (autoload 'pymacs-eval "pymacs" nil t)
-;; 	 (autoload 'pymacs-exec "pymacs" nil t)
-;; 	 (autoload 'pymacs-load "pymacs" nil t)
-;; 	 (message "loading ropemacs")
-;; 	 (pymacs-load "ropemacs" "rope-")
-;; 	 (setq ropemacs-enable-autoimport t)))
-;;; integrate ropemacs with company-mode
-;; (defun company-ropemacs-setup ()
-;;   (add-to-list 'company-backend 'company-ropemacs))
-;; (add-hook 'python-mode-hook 'company-ropemacs-setup)
+(eval-after-load 'python
+  '(progn
+	 (autoload 'pymacs-apply "pymacs")
+	 (autoload 'pymacs-call "pymacs")
+	 (autoload 'pymacs-eval "pymacs" nil t)
+	 (autoload 'pymacs-exec "pymacs" nil t)
+	 (autoload 'pymacs-load "pymacs" nil t)
+	 (message "loading ropemacs")
+	 (pymacs-load "ropemacs" "rope-")
+	 (setq ropemacs-enable-autoimport t)))
+integrate ropemacs with company-mode
+(defun company-ropemacs-setup ()
+  (add-to-list 'company-backend 'company-ropemacs))
+(add-hook 'python-mode-hook 'company-ropemacs-setup)
 
 (provide 'init-python-mode)
