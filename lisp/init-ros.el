088474e5efe1473dd7b166ca20242c01b3d9cbb7
Chao Liu <chaoliu@seas.upenn.edu>
2017-08-14 13:08:06 -0400

    Unbind C-x C-r C-l for redshank-mode used for ros-launch

--- lisp/init-ros.el
+++ lisp/init-ros.el
@@ -2,6 +2,10 @@
 
 (require 'rosemacs-config)
 
+(after-load 'rosemacs
+  (require 'redshank)
+  (define-key redshank-mode-map (kbd "C-x C-r C-l") nil))
+
 (require-package 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
