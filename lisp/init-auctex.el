eb2a1e75f090731464fb3c3a76af6229d1d00571
Chao Liu <chao.liu0307@gmail.com>
2017-08-31 20:40:06 -0400

    Use pdflatex as default compiler for tex file on Ubuntu
    Also remove some unused auctex configurations.

--- lisp/init-auctex.el
+++ lisp/init-auctex.el
@@ -46,12 +46,6 @@
   )
 
 (when *is-a-linux*
-  (eval-after-load "tex-mode"
-	'(progn
-	   (load "auctex.el" nil t t)
-	   ;; (load "preview-latex.el" nil t t)
-	   )
-	)
 
   (setq TeX-auto-save t)
   (setq TeX-parse-self t)
@@ -61,8 +55,8 @@
 
   (add-hook 'LaTeX-mode-hook
 			(lambda()
-			  (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex --synctex=1%(mode)%' %t" TeX-run-TeX nil t))
-			  (setq TeX-command-default "XeLaTeX")))
+			  (add-to-list 'TeX-command-list '("PDFLaTeX" "%`pdflatex --synctex=1%(mode)%' %t" TeX-run-TeX nil t))
+			  (setq TeX-command-default "PDFLaTeX")))
 
   (mapc (lambda (mode)
 		  (add-hook 'LaTeX-mode-hook mode))
