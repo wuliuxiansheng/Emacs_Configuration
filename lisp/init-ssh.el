382c1c0498f4fb76aa2851fd7d2711892cf7ed5b
Chao Liu <chaoliu@seas.upenn.edu>
2017-10-21 11:43:14 -0400

    remove one item for tramp-parse-sconfig

--- lisp/init-ssh.el
+++ lisp/init-ssh.el
@@ -1,10 +1,8 @@
 ;;; ssh configuration
-
 (require 'tramp)
 
 (tramp-set-completion-function "ssh"
-							   '((tramp-parse-sconfig "/etc/ssh_config")
-								 (tramp-parse-sconfig "~/.ssh/config")))
+							   '((tramp-parse-sconfig "~/.ssh/config")))
 
 (setq tramp-default-method "ssh")
 
