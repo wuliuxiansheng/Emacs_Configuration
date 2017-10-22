59e18474ba72b1ab196ef873fab8b8e438717e13
Chao Liu <chao.liu0307@gmail.com>
2017-10-21 21:23:08 -0400

    Fix misalignment for company completion with fci-mode on

--- lisp/init-fci.el
+++ lisp/init-fci.el
@@ -36,5 +36,20 @@
       (when (sanityinc/fci-enabled-p)
         (turn-on-fci-mode)))))
 
+;; Fix misalignment for company completion with fci-mode on
+(defvar-local company-fci-mode-on-p nil)
+
+(defun company-turn-off-fci (&rest ignore)
+  (when (boundp 'fci-mode)
+    (setq company-fci-mode-on-p fci-mode)
+    (when fci-mode (fci-mode -1))))
+
+(defun company-maybe-turn-on-fci (&rest ignore)
+  (when company-fci-mode-on-p (fci-mode 1)))
+
+(add-hook 'company-completion-started-hook 'company-turn-off-fci)
+(add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
+(add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci)
+
 
 (provide 'init-fci)
