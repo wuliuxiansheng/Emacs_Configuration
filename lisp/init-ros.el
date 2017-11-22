86a37417ea9c44be91ed93e4a2794a9c09ad6d52
Chao Liu <chaoliu@seas.upenn.edu>
2017-11-22 15:01:26 -0500

    Add catkin_make command to compile ros code

--- lisp/init-ros.el
+++ lisp/init-ros.el
@@ -10,4 +10,15 @@
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 
+;; configure catkin_make command
+(defun ros-catkin-make (dir)
+  "Run catkin_make command in DIR."
+  (interactive (list (read-directory-name "Directory: ")))
+  (let* ((default-directory dir)
+		 (compilation-buffer-name-function (lambda (major-mode-name) "*catkin_make*")))
+	(compile "catkin_make -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ."))
+  )
+
+(global-set-key (kbd "C-x C-r M") 'ros-catkin-make)
+
 (provide 'init-ros)
