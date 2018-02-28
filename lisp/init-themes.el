f941f0f1a0914181095650dd7a49d4c60f33fc57
Chao Liu <chaoliu@seas.upenn.edu>
2018-02-27 21:15:06 -0500

    Enable dimmer mode by default

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -35,7 +35,9 @@
   (color-theme-sanityinc-solarized-dark)
   (reapply-themes))
 
-(maybe-require-package 'dimmer)
+(when (maybe-require-package 'dimmer)
+  (setq-default dimmer-fraction 0.1)
+  (add-hook 'after-init-hook 'dimmer-mode))
 
 ;;------------------------------------------------------------------------------
 ;; Theme Configuration for Org-mode
