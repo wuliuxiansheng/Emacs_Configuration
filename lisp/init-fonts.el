f1a921e15480f84bf4399158570fa99c05613aca
Chao Liu <chaoliu@seas.upenn.edu>
2018-03-13 21:07:24 -0400

    Update font size for Ubuntu system

--- lisp/init-fonts.el
+++ lisp/init-fonts.el
@@ -30,7 +30,7 @@ This is helpful for writeroom-mode, in particular."
   ;; configure Chinese characters to align tables
   (dolist (charset '(kana han symbol cjk-misc bopomofo))
     (set-fontset-font (frame-parameter nil 'font)
-                      charset (font-spec :family "Noto Sans CJK JP" :size 18))))
+                      charset (font-spec :family "Noto Sans CJK JP" :size 16))))
 (when *is-a-windows*
   (set-frame-font "-outline-Courier New-normal-normal-normal-mono-14-*-*-*-c-*-iso8859-1")
   ;; configure Chinese characters to align tables
