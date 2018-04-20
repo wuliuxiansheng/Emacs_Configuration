5814f96a567983ae73415cc0708a6922feef2ec4
Chao Liu <chaoliu@seas.upenn.edu>
2018-04-19 22:54:54 -0400

    Add company-arduino configuration

--- lisp/init-arduino.el
+++ lisp/init-arduino.el
@@ -1,5 +1,27 @@
 (require-package 'arduino-mode)
-
 (setq ede-arduino-appdir "~/arduino-ide")
+
+(require-package 'company-arduino)
+
+(defun arduino-enable ()
+  "Start arduino mode."
+  (interactive)
+  (add-to-list 'irony-supported-major-modes 'arduino-mode)
+  (add-hook 'irony-mode-hook 'company-arduino-turn-on)
+  (add-to-list 'company-c-headers-path-system 'company-arduino-includes-dirs)
+  (add-hook 'arduino-mode-hook 'irony-mode)
+  (add-hook 'arduino-mode-hook 'company-irony-setup)
+  (add-hook 'arduino-mode-hook 'company-c-headers-setup))
+
+(defun arduino-disable ()
+  "Disable arduino mode."
+  (interactive)
+  (delete 'arduino-mode irony-supported-major-modes)
+  (remove-hook 'irony-mode-hook 'company-arduino-turn-on)
+  (delete 'company-arduino-includes-dirs company-c-headers-path-system)
+  (remove-hook 'arduino-mode-hook 'irony-mode)
+  (remove-hook 'arduino-mode-hook 'company-irony-setup)
+  (remove-hook 'arduino-mode-hook 'company-c-headers-setup))
+
 
 (provide 'init-arduino)
