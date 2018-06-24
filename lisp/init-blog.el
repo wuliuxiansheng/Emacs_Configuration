6241d586113dccbde2e5261cf4f173fa1c2102c1
Chao Liu <chaoliu@seas.upenn.edu>
2018-06-23 22:30:59 -0400

    Update blog configuration
    Update blog path and configure shell-command for Ubuntu so that Emacs
    shell-command can find hexo command

--- lisp/init-blog.el
+++ lisp/init-blog.el
@@ -3,10 +3,16 @@
 
 (require 'blog-admin)
 
-(setq blog-admin-backend-path "~/Dropbox/Personal/File/blog")
+(setq blog-admin-backend-path "~/Documents/blog")
 (setq blog-admin-backend-type 'hexo)
 (setq blog-admin-backend-new-post-in-drafts t) ;; create new post in drafts by default
 (setq blog-admin-backend-new-post-with-same-name-dir t) ;; create same-name directory with new post
 (setq blog-admin-backend-hexo-config-file "_config.yml") ;; default assumes _config.yml
+
+(defun blog-shell-command ()
+  "Make \"shell-command\" to be interactive for blog mode."
+  (setq shell-command-switch "-ic"))
+(when *is-a-linux*
+  (add-hook 'blog-admin-mode-hook 'blog-shell-command))
 
 (provide 'init-blog)
