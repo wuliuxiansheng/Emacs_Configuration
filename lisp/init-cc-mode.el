8d767e8c12f7c199d82f39eb3624980550550723
Chao Liu <chaoliu@seas.upenn.edu>
2018-07-17 09:54:11 -0400

    Add more offset setting for cc-mode

--- lisp/init-cc-mode.el
+++ lisp/init-cc-mode.el
@@ -19,7 +19,8 @@
   (c-set-offset 'innamespace 0)
   (c-set-offset 'inclass 4)
   (c-set-offset 'access-label -2)
-  (c-set-offset 'defun-block-intro 4))
+  (c-set-offset 'defun-block-intro 4)
+  (c-set-offset 'statement-block-intro 4))
 
 (add-hook 'c-mode-hook 'cc-common-setup)
 (add-hook 'c++-mode-hook 'cc-common-setup)
