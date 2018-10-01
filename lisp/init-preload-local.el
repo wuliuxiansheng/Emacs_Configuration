bd3a9c872469964ba5387cd3d4b0fa75516ed817
Chao Liu <chaoliu@seas.upenn.edu>
2018-09-30 22:23:07 -0400

    Update configuration for line number display

--- lisp/init-preload-local.el
+++ lisp/init-preload-local.el
@@ -1,5 +1,3 @@
-;;;set the default font and size
-
 (when *is-a-mac*
   ;; (setq ring-bell-function 'ignore);turn the alarm totally off because of the bug in Capitan EI
   )
@@ -9,15 +7,6 @@
   (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/")  ; add aspell directory to emacs configuration
   (setq grep-find-command '("findstr /sn  *" . 13))
   (setq default-directory "~/"))
-
-;; line number configuration
-(global-linum-mode t);show line number
-;; disable line number display for some modes
-(setq linum-disabled-modes-list '(eshell-mode shell-mode term-mode org-mode text-mode inferior-python-mode TeX-output-mode compilation-mode comint-mode blog-admin-mode gnus-group-mode gnus-article-mode bbdb-mode))
-(defun linum-on ()
-  (unless (or (minibufferp)
-			  (member major-mode linum-disabled-modes-list))
-	(linum-mode 1)))
 
 ;;;cursor stop blinking
 (blink-cursor-mode -1)
