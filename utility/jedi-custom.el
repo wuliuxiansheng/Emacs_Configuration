e66811936fde08a0391297aca0e3c80410f823f3
Chao Liu <chaoliu@seas.upenn.edu>
2019-04-06 19:23:46 -0400

    Update jedi customization file

--- utility/jedi-custom.el
+++ utility/jedi-custom.el
@@ -1,4 +1,8 @@
-;;; Python project configuration for Jedi
+;;; Jedi customization
 
 (setq jedi:server-args
-	  '("--sys-path" "/home/chaoliu/Documents/smores/reconfiguration/software/python/"))
+      '("--sys-path" "/usr/local/lib/python2.7/dist-packages"
+		"--sys-path" "/opt/ros/melodic/lib/python2.7/dist-packages"
+        "--sys-path" "/home/chaoliu/Documents/tether-tube_robots_ros/src/ttr_control/scripts"
+        "--sys-path" "/home/chaoliu/Documents/tether-tube_robots_ros/src/ttr_robot/scripts"
+        "--sys-path" "/home/chaoliu/pyckbot/py"))
