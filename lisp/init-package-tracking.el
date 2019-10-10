049299459269bfd07e11e08e3e6bdde7c769965a
Chao Liu <chaoliu@seas.upenn.edu>
2019-10-10 17:30:57 -0400

    Fix the error in init-package-tracking.el

--- lisp/init-package-tracking.el
+++ lisp/init-package-tracking.el
@@ -1,13 +1,11 @@
-(require 'pkg-info)
+(require-package 'pkg-info)
 
 ;; record all installed packages with name+version in a file
 (defun record-current-package ()
   "Record the current package in format package: verison and save the data in ~/.emacs.d/package-tracking.el."
   (interactive)
   (dolist (pkg package-activated-list)
-    (message "package %s" pkg)
     (setq pkg-string (format "%s" pkg))
-    (message "package string %s" pkg-string)
     (append-to-file (format "%s: %s\n" pkg-string (pkg-info-format-version (pkg-info-package-version pkg))) nil "~/.emacs.d/package-tracking.el")))
 
 (provide 'init-package-tracking)
