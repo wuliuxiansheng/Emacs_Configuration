477ac0d20491a02631139ec88d39ccbfa1e5ee75
Chao Liu <chaoliu@seas.upenn.edu>
2020-04-22 17:35:02 -0400

    Add configuration to open files using external default applications

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -17,4 +17,60 @@
   (after-load 'dired
     (add-hook 'dired-mode-hook 'diff-hl-dired-mode)))
 
+;; Open files using OS default applications
+(defun file-open-in-external-app (&optional @fname)
+  "Open the current file or dired marked files in external app.
+The app is chosen from your OS's preference.
+
+When called in emacs lisp, if @fname is given, open that.
+
+URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
+Version 2019-11-04"
+  (interactive)
+  (message @fname)
+  (let* (
+         ($file-list
+          (if @fname
+              (progn (list @fname))
+            (if (string-equal major-mode "dired-mode")
+                (dired-get-marked-files)
+              (list (buffer-file-name)))))
+         ($do-it-p (if (<= (length $file-list) 5)
+                       t
+                     (y-or-n-p "Open more than 5 files? "))))
+    (when $do-it-p
+      (cond
+       ((string-equal system-type "windows-nt")
+        (mapc
+         (lambda ($fpath)
+           (w32-shell-execute "open" $fpath)) $file-list))
+       ((string-equal system-type "darwin")
+        (mapc
+         (lambda ($fpath)
+           (shell-command
+            (concat "open " (shell-quote-argument $fpath))))  $file-list))
+       ((string-equal system-type "gnu/linux")
+        (mapc
+         (lambda ($fpath) (let ((process-connection-type nil))
+                       (start-process "" nil "xdg-open" $fpath))) $file-list))))))
+
+;; Open current directory in Mac Finder, Windows Explorer or Linux file manager
+(defun file-show-in-desktop ()
+  "Show current file in desktop.
+ (Mac Finder, Windows Explorer, Linux file manager)
+ This command can be called when in a file or in `dired'."
+  (interactive)
+  (file-open-in-external-app (expand-file-name default-directory))
+  )
+
+;; (defun file-open-by-default ()
+;;   "Open file using default external application."
+;;   (local-set-key (kbd "C-c o") 'file-open-in-external-app)
+;;   (local-set-key (kbd "C-c s") 'file-show-in-desktop))
+
+;; (add-hook 'dired-mode-hook 'file-open-by-default)
+
+(global-set-key (kbd "C-c o") 'file-open-in-external-app)
+(global-set-key (kbd "C-c s") 'file-show-in-desktop)
+
 (provide 'init-dired)
