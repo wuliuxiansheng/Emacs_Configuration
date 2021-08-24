0e0e2bf8e03245caf70615b806bff51cc48d0761
Chao Liu <chaoliu@seas.upenn.edu>
2021-08-24 14:42:31 -0400

    Forget to add customized jedi configuration when using company-jedi

--- utility/jedi-custom.el
+++ utility/jedi-custom.el
@@ -1,5 +1,6 @@
 ;;; Jedi customization
 
+;; customization for jedi using company-jedi
 (setq jedi:server-args
       '("--sys-path-append" "/usr/local/lib/python2.7/dist-packages"
         "--sys-path-append" "/opt/ros/melodic/lib/python2.7/dist-packages"
@@ -7,6 +8,8 @@
         "--sys-path-append" "/home/chaoliu/Documents/tether-tube_robots_ros/src/ttr_robot/scripts"
         "--sys-path-append" "/home/chaoliu/pyckbot/py"))
 
+(setq jedi:server-command
+      '("/usr/bin/python3" "/home/chaoliu/.emacs.d/elpa-26.3/jedi-core-20210503.1315/jediepcserver.py"))
 
 ;; customization for jedi used by lsp
 (with-eval-after-load 'lsp-mode
