63651bd97def628e3f29a4fc3f9d71593e4fc960
Chao Liu <chaoliu@csail.mit.edu>
2021-12-22 17:14:16 -0500

    Add configurations for mu4e

--- lisp/init-mail.el
+++ lisp/init-mail.el
@@ -11,5 +11,24 @@
 ;; (setq mm-inline-text-html-with-images t)
 ;; (setq w3m-default-display-inline-images t)
 
+(require 'mu4e)
+(setq mu4e-headers-include-related t)
+(setq mu4e-change-filename-when-moving t)
+
+(setq mu4e-get-mail-command "mbsync -a")
+
+(setq mu4e-maildir "~/.mail/Gmail")
+(setq mu4e-drafts-folder "/Drafts")
+(setq mu4e-sent-folder "/Sent")
+(setq mu4e-refile-folder "/All Mail")
+(setq mu4e-trash-folder "/Trash")
+
+;; SMTP configuration
+(setq send-mail-function 'smtpmail-send-it)
+(setq smtpmail-smtp-server "smtp.gmail.com")
+(setq smtpmail-smtp-service 587)
+(setq smtpmail-stream-type 'starttls)
+
+
 
 (provide 'init-mail)
