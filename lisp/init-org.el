00ba34888dd8c2856e28923cd0ae0c7283b20a1f
Chao Liu <chaoliu@csail.mit.edu>
2022-01-10 19:51:14 -0500

    Comment C-c o keybinding for org
    This keybinding is used for open documents in dired mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -107,7 +107,7 @@
 (define-key sanityinc/org-global-prefix-map (kbd "l") 'org-clock-in-last)
 (define-key sanityinc/org-global-prefix-map (kbd "i") 'org-clock-in)
 (define-key sanityinc/org-global-prefix-map (kbd "o") 'org-clock-out)
-(define-key global-map (kbd "C-c o") sanityinc/org-global-prefix-map)
+;; (define-key global-map (kbd "C-c o") sanityinc/org-global-prefix-map)
 
 (setq org-startup-indented t)
 
