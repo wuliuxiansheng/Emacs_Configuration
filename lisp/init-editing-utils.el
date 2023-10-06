3165f97f702a9f7bd1c2bb1df867607fe1f4ffb3
Chao Liu <chaoliu@csail.mit.edu>
2023-10-05 22:25:35 -0400

    Ignore gpg files for undo

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -127,6 +127,15 @@
 (after-load 'undo-tree
   (diminish 'undo-tree-mode))
 
+(defun fov/disable-backups-for-gpg ()
+  "Disable backups and autosaving for files ending in \".gpg\"."
+  (when (and (buffer-file-name)
+             (s-ends-with-p ".gpg" (buffer-file-name) t))
+    (setq-local backup-inhibited t)
+    (setq-local undo-tree-auto-save-history nil)
+    (auto-save-mode -1)))
+(add-hook 'find-file-hook #'fov/disable-backups-for-gpg)
+
 
 (when (maybe-require-package 'symbol-overlay)
   (dolist (hook '(prog-mode-hook html-mode-hook yaml-mode-hook conf-mode-hook))
