c0fa647e8f65c450af02d8928ce1bef11dbf5a60
Chao Liu <chaoliu@csail.mit.edu>
2024-07-04 23:42:12 -0400

    Add configuration for gptel to interact with LLM

---
+++ lisp/init-llm.el
@@ -0,0 +1,32 @@
+;;; gptel configuration
+(require-package 'gptel)
+
+;; (gptel-make-ollama "Ollama"             ;Any name of your choosing
+;;   :host "localhost:11434" ;Where it's running
+;;   :stream t               ;Stream responses
+;;   :models '("llama3:latest", "llama3:text"))          ;List of models
+
+(setq
+ gptel-model "llama3:latest"
+ gptel-backend (gptel-make-ollama "Ollama"
+                 :host "localhost:11434"
+                 :stream t
+                 :models '("llama3:latest", "llama3:text")))
+
+(setq gptel-default-mode 'org-mode)
+
+;; Customize the buffer for gptel
+;; (add-hook 'gptel-mode-hook 'turn-on-visual-line-mode)
+(add-hook 'gptel-mode-hook
+          (lambda ()
+            (setq-local truncate-lines nil)
+            (setq-local word-wrap t)
+            (setq-local line-spacing 0.2)
+            (flyspell-mode 1)
+            (turn-on-visual-line-mode)
+            ))
+
+;; Move cursor to the next prompt
+(add-hook 'gptel-post-response-functions 'gptel-end-of-response)
+
+(provide 'init-llm)
