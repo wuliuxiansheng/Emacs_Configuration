636aaf487b2194903981b3e8dd1f2f7716ed295e
Chao Liu <chao.liu@ubc.ca>
2026-04-17 13:38:27 -0700

    Add configurations for Claude Code

--- lisp/init-llm.el
+++ lisp/init-llm.el
@@ -29,4 +29,21 @@
 ;; Move cursor to the next prompt
 (add-hook 'gptel-post-response-functions 'gptel-end-of-response)
 
+
+;; Claude Code configuration
+(require-package 'vterm)
+(require-package 'eat)
+(eat-eshell-mode)
+
+(use-package claude-code-ide
+  :vc (:url "https://github.com/manzaltu/claude-code-ide.el" :rev :newest)
+  :bind ("C-c C-'" . claude-code-ide-menu) ; Set your favorite keybinding
+  :config
+  (claude-code-ide-emacs-tools-setup) ; Optionally enable Emacs MCP tools
+  :custom
+  (claude-code-ide-window-width 80)
+  (claude-code-ide-terminal-backend 'vterm)
+  (claude-code-ide-vterm-render-delay 0.01)
+  (claude-code-ide-focus-on-open nil))
+
 (provide 'init-llm)
